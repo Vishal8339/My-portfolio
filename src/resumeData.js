0f4c91e6a0ae0fbcad05c48f92155bbf0a46f5a4
Vishal8339 <130916982+Vishal8339@users.noreply.github.com>
2023-05-02 20:52:30 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+  "imagebaseurl": "https://vishal8339.github.io/",
+  "name": "anuj kumar verma",
+  "role": "Full stack developer",
+  "linkedinId": "student life anuj",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/student-life-anuj-bb53a521a",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/vishal8339",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/Anujkumarv32523",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme":"Hey there, I'm a under graduate student of B.Sc.(PCM). I want to be a full stack web developer. ",
+  "address": "Khushhalpur,Barabanki,UttarPradesh,INDIA",
+  "website": "https://vishal8339.github.io",
+  "education": [
+    {
+      "UniversityName": "Dr.Ram Manohar Lohia Avadh University",
+      "specialization": "Average student",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2024",
+      "Achievements": "There is no any special achievement"
+    },
+    // {
+    //   "UniversityName":"Some University",
+    //   "specialization":"Some specialization",
+    //   "MonthOfPassing":"Jan",
+    //   "YearOfPassing":"2018",
+    //   "Achievements":"Some Achievements"
+    // }
+  ],
+  "work": [
+    {
+      "CompanyName": "At Home ",
+      "specialization": "Learning , To be a full stack web developer",
+    }
+
+    // {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "image.png"
+    },
+    // {
+    //   "name":"project2",
+    //   "description":"mobileapp",
+    //   "imgurl":"images/portfolio/project.jpg"
+    // },
+    // {
+    //   "name":"project3",
+    //   "description":"mobileapp",  
+    //   "imgurl":"images/portfolio/project2.png"
+    // },
+    // {
+    //   "name":"project4",
+    //   "description":"mobileapp",
+    //   "imgurl":"images/portfolio/phone.jpg"
+    // }
+  ],
+  "testimonials": [
+    {
+      "description": "When Nothing Gone Right In Your Life Go To Sleep",
+      "name": "It's Anuj"
+    },
+    // {
+    //   "description":"This is a sample testimonial",
+    //   "name":"Some technical guy"
+    // }
+  ]
+}
+
+export default resumeData
